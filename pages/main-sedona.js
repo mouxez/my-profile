c804d817eef53fd696bd99e135e7b09a116742f7
Maksim <mouxez@gmail.com>
2019-10-15 14:59:03 +0300

    scripts min

--- pages/main-sedona.js
+++ pages/main-sedona.js
@@ -1,14 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var hotelSearch = document.querySelector('.hotel-search');
-  var hotelSearchForm = hotelSearch.querySelector('.hotel-search-form');
-
-  hotelSearchForm.classList.remove('hotel-search-form-active');
-
-  hotelSearch.addEventListener('click', function (event) {
-      var target = event.target;
-
-      if (target.classList.contains('main-button')) {
-          hotelSearchForm.classList.toggle('hotel-search-form-active');
-      }
-  });
-});
+document.addEventListener('DOMContentLoaded',function(){var hotelSearch=document.querySelector('.hotel-search');var hotelSearchForm=hotelSearch.querySelector('.hotel-search-form');hotelSearchForm.classList.remove('hotel-search-form-active');hotelSearch.addEventListener('click',function(event){var target=event.target;if(target.classList.contains('main-button')){hotelSearchForm.classList.toggle('hotel-search-form-active')}})})
